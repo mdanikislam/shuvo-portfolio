a15d284cb91a7e78c08d05ffc77b7f886bdcb98d
mdanikislam <anikislam487@gmail.com>
2025-07-13 22:16:10 +0600

    about me update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,33 +1,33 @@
 import aboutPic from "../assets/images/about.jpg";
 const About = () => {
   return (
-    <div className="py-10 w-9/12 mx-auto">
+    <div className="py-10 md:w-9/12 w-10/12 mx-auto">
       <div>
         <img
           className=" w-6/12 md:w-3/12 mx-auto"
           src={aboutPic}
           alt="About_Me"
         />
-        <h1 className="md:text-4xl text-2xl font-bold text-center pt-4 text-purple-600">
+        <h1 className="md:text-5xl text-2xl font-bold text-center pt-4 text-purple-600">
           Ranjohn Sarkar Shuvo
         </h1>
+        <hr className="mt-5 border-1 text-purple-700 md:w-xs mx-auto" />
+        <p className="pt-2 text-gray-600 text-center">"Data-driven strategy. Creative execution. Measurable results."</p>
       </div>
       <div className="pt-5">
         <p className="text-xl text-center">
-          A passionate Digital Marketing Executive based in Dhaka, Bangladesh. I
-          specialize in creating effective marketing strategies that drive
-          results and help brands grow online.
+          I believe marketing should be both art and science. I love blending creative storytelling with analytics to deliver campaigns that don’t just look good but perform well.
         </p>
 
         <p className="text-xl pt-8 text-center">
           I combine creativity, analytics, and a deep understanding of digital
           platforms to design campaigns that connect with audiences. With
           experience in managing Facebook Ads, content marketing, and SEO, I
-          focus on delivering measurable business outcomes for my clients. I’m
-          always eager to learn and adapt to the rapidly evolving digital
-          landscape, staying updated with the latest tools, trends, and best
-          practices.
+          focus on delivering measurable business outcomes for my clients.
         </p>
+      </div>
+      <div>
+        <h1>Key Skills</h1>
       </div>
     </div>
   );
