c89e5d98b5d1e9cdeeeec870ce97fe78d54dfe86
mdanikislam <anikislam487@gmail.com>
2025-07-20 19:27:02 +0600

    blog img done

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -47,17 +47,17 @@ const Blog = () => {
 				</div>
 			</a>
 			<a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
-				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?5" />
+				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://www.digitalsocialite.in/wp-content/uploads/2023/11/8-Digital-Skills-That-Will-Increase-Your-Salary.webp" />
 				<div className="p-6 space-y-2">
-					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
+					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">8 Digital Skills That Will Increase Your Salary</h3>
 					<span className="text-xs dark:text-gray-600">January 25, 2021</span>
 					<p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
 				</div>
 			</a>
 			<a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
-				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?6" />
+				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://mydmi.imgix.net/Blog_Card_12_Examples_of_Powerful_Brand_Collaborations.png?crop=edges&fit=crop&fm=auto&h=1260&ixlib=php-4.1.0&q=40&w=2400&s=85fd3d8c4c6d5e31ff1ab694cb8448c9" />
 				<div className="p-6 space-y-2">
-					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
+					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">12 Examples of Powerful Brand Collaborations</h3>
 					<span className="text-xs dark:text-gray-600">January 26, 2021</span>
 					<p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
 				</div>
