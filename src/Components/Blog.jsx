527c6c01f5ac58e8720760d476d0364bf2de3783
mdanikislam <anikislam487@gmail.com>
2025-07-20 19:21:17 +0600

    email

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -6,9 +6,9 @@ const Blog = () => {
             <section className="dark:bg-gray-100 dark:text-gray-800">
 	<div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
 		<a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
-			<img src="https://source.unsplash.com/random/480x360" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+			<img src="https://magemail.co/wp-content/uploads/2024/08/Email-Marketing.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
 			<div className="p-6 space-y-2 lg:col-span-5">
-				<h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
+				<h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">The Marketer’s Guide to Creating Great Email Newsletters</h3>
 				<span className="text-xs dark:text-gray-600">February 19, 2021</span>
 				<p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
 			</div>
